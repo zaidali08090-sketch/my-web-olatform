235f2a009ea02a8199d740c1c2798cb262c4d587
EN_CLUB <zaidali08090@gmail.com>
2026-04-02 03:58:16 -0700

    Create constants file

---
+++ src/config/constants.js
@@ -0,0 +1,34 @@
+// Sections
+export const SECTIONS = ['Literature', 'Linguistics', 'Others'];
+
+// Exam Types
+export const EXAM_TYPES = ['Mid', 'Final', 'Quiz'];
+
+// Skills Levels
+export const SKILL_LEVELS = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2'];
+
+// Database Collections
+export const COLLECTIONS = {
+    USERS: 'users',
+    VIDEOS: 'videos',
+    MATERIALS: 'materials',
+    SUMMARIES: 'summaries',
+    NOVELS: 'novels',
+    PODCASTS: 'podcasts',
+    EXAMS: 'exams',
+    SUBJECTS: 'subjects',
+    DOCTORS: 'doctors',
+    MESSAGES: 'messages',
+    USER_LEVELS: 'user_levels',
+};
+
+// Admin Email
+export const ADMIN_EMAIL = 'zaidali08090@gmail.com';
+
+// Doctors Data
+export const DOCTORS_DATA = {
+    Linguistics: ['Dr. Ghada AlKarazoun', 'Dr. Omar'],
+    Literature: ['Dr. Abdulhadi', 'Dr. Ghada Abu Al-Enain', 'Dr. Shatha'],
+    Methods: ['Dr. Amal'],
+    All: ['Dr. Ghada AlKarazoun', 'Dr. Omar', 'Dr. Abdulhadi', 'Dr. Ghada Abu Al-Enain', 'Dr. Shatha', 'Dr. Amal'],
+};
